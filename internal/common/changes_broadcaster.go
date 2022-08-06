784c9e7366fc30e2cb19e64222a4d5d420758e7a
sarpt <mike.m.info@gmail.com>
2022-08-06 13:10:26 +0200

    change broadcaster to ptr in changes broadcaster

--- internal/common/changes_broadcaster.go
+++ internal/common/changes_broadcaster.go
@@ -1,30 +1,21 @@
 package common
 
-import (
-	"sync"
-)
-
 type ChangeVariant string
 
 type Change interface {
 	Variant() ChangeVariant
 }
 
-// type ChangesSubscriber = func(change interface{})
 type ChangesSubscriber[CT Change] interface {
 	Receive(change CT)
 }
 
 type ChangesBroadcaster[CT Change] struct {
-	Broadcaster[CT]
+	*Broadcaster[CT]
 }
 
 func NewChangesBroadcaster[CT Change]() *ChangesBroadcaster[CT] {
 	return &ChangesBroadcaster[CT]{
-		Broadcaster[CT]{
-			changes:     make(chan CT),
-			lock:        &sync.RWMutex{},
-			subscribers: []Subscriber[CT]{},
-		},
+		NewBroadcaster[CT](),
 	}
 }
